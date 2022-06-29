c5090908855fce0d9feac0a1bb9c0234b83f3f78
AuksaraOun <auksaraoun@hotmail.com>
2022-06-29 17:47:50 +0700

    add API Category Auth

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Category;
+use Validator;
 
 class CategoryController extends Controller
 {
@@ -13,19 +14,27 @@ class CategoryController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        return Category::get();
-    }
+        $per_page = $request->input('per_page', 50);
+        $sortBy = $request->input('sortBy', 'id');
+        $sortDesc = $request->input('sortDesc', 'asc');
+        $search = $request->input('search');
+        $category_id = $request->input('category_id');
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
+        $categories = Category::select('categories.*');
+
+        if ($search) {
+            $categories->where(function ($query) {
+                $query->where('name', 'like', "%$search%");
+            });
+        }
+
+        $categories->orderBy($sortBy, $sortDesc);
+        return response()->json([
+            'status' => true,
+            'categories' => $categories->paginate($per_page)
+        ], 200);
     }
 
     /**
@@ -36,7 +45,53 @@ class CategoryController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Validation fail',
+                'errors' => $validator->errors()
+            ], 401);
+        }
+
+        $category_id = $request->input('category_id');
+
+        $main_category = Category::find($category_id);
+
+        if ($category_id && !$main_category) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Not found Category ID = ' . $category_id . ' in our system',
+            ], 401);
+        }
+
+        if ($main_category && $main_category->level == 3) {
+            return response()->json([
+                'status' => false,
+                'message' => 'You can have 3 level category',
+            ], 401);
+        }
+
+        try {
+            $category = Category::create([
+                'category_id' => $request->input('category_id'),
+                'name' => $request->input('name'),
+                'level' => $main_category ? $main_category->level + 1 : 1
+            ]);
+            return response()->json([
+                'status' => true,
+                'message' => 'Create category success',
+                'category' => $category
+            ]);
+        } catch (\Throwable $th) {
+            return response()->json([
+                'status' => false,
+                'message' => $th->getMessage()
+            ], 500);
+        }
     }
 
     /**
@@ -47,18 +102,10 @@ class CategoryController extends Controller
      */
     public function show($id)
     {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
+        return response()->json([
+            'status' => false,
+            'category' => Category::find($id)
+        ]);
     }
 
     /**
@@ -70,7 +117,60 @@ class CategoryController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Validation fail',
+                'errors' => $validator->errors()
+            ], 401);
+        }
+
+        $category_id = $request->input('category_id');
+
+        $main_category = Category::find($category_id);
+
+        if ($category_id && !$main_category) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Not found Category ID = ' . $category_id . ' in our system',
+            ], 401);
+        }
+
+        if ($main_category && $main_category->level == 3) {
+            return response()->json([
+                'status' => false,
+                'message' => 'You can have 3 level category',
+            ], 401);
+        }
+
+        try {
+            $old_category = Category::find($id);
+            $level = 1;
+            if ($category_id && $old_category->category_id == $category_id) {
+                $level = $old_category->level;
+            } elseif ($category_id && $old_category->category_id != $main_category->id) {
+                $level = $main_category->level + 1;
+            }
+            $category = Category::where('id', $id)->update([
+                'category_id' => $request->input('category_id'),
+                'name' => $request->input('name'),
+                'level' => $level
+            ]);
+            return response()->json([
+                'status' => true,
+                'message' => 'Update category success',
+                'category' => $category
+            ]);
+        } catch (\Throwable $th) {
+            return response()->json([
+                'status' => false,
+                'message' => $th->getMessage()
+            ], 500);
+        }
     }
 
     /**
@@ -81,6 +181,10 @@ class CategoryController extends Controller
      */
     public function destroy($id)
     {
-        //
+        Category::find($id)->delete();
+        return response()->json([
+            'status' => true,
+            'message' => 'Delete category',
+        ]);
     }
 }
